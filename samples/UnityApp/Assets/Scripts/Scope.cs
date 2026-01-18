0cdcfed8084de64ab81450857faf866f4fa4fe59
Eugene Cheshenkov <e.cheshenkov@gmail.com>
2026-01-18 13:05:12 +0300

    Example CompositionKind.Internal and RootKinds.Exposed using ScriptableObject

--- samples/UnityApp/Assets/Scripts/Scope.cs
+++ samples/UnityApp/Assets/Scripts/Scope.cs
@@ -1,14 +1,33 @@
 using Pure.DI;
 using UnityEngine;
 
-public partial class Scope : MonoBehaviour
+[CreateAssetMenu(menuName = "Clock/Clocks Composition", fileName = "ClocksComposition", order = 0)]
+public partial class ClocksComposition : ScriptableObject
 {
     [SerializeField] public ClockConfig clockConfig;
 
-    void Setup() => DI.Setup()
-        .SpecialType<MonoBehaviour>()
+    // #1 Variant
+    void Setup() => DI.Setup(nameof(ClocksComposition), kind: CompositionKind.Internal)
+        .SpecialType<ScriptableObject>()
         .Transient(() => clockConfig)
-        .Singleton<ClockService>()
+        .Singleton<ClockService>();
+
+    // #2 Variant
+    // void Setup() => DI.Setup()
+    //     .SpecialType<ScriptableObject>()
+    //     .Transient(() => clockConfig)
+    //     .Singleton<ClockService>()
+    //     .Root<IClockService>(nameof(IClockService), kind: RootKinds.Exposed);
+}
+
+public partial class Scope : MonoBehaviour
+{
+    [SerializeField] public ClocksComposition clocksComposition;
+
+    void Setup() => DI.Setup()
+        .DependsOn(nameof(ClocksComposition)) // #1 Variant
+        // .Bind().To(_ => clocksComposition) // #2 Variant
+        .SpecialType<MonoBehaviour>()
         .Root<ClockManager>(nameof(ClockManager))
         .Builders<MonoBehaviour>();
 
