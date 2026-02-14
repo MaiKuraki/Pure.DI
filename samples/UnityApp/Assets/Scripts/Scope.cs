983a9321ea3980169383dbdf13a882c527f62bac
Nikolay Pianikov <nikolayp@mail.ru>
2026-02-14 09:13:50 +0300

    Update Unity project to use variant 1

--- samples/UnityApp/Assets/Scripts/Scope.cs
+++ samples/UnityApp/Assets/Scripts/Scope.cs
@@ -2,34 +2,20 @@ using Pure.DI;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Clock/Clocks Composition", fileName = "ClocksComposition", order = 0)]
-public partial class ClocksComposition : ScriptableObject
+class ClocksComposition : ScriptableObject
 {
-    [SerializeField] public ClockConfig clockConfig;
+    [SerializeField] private ClockConfig clockConfig;
 
-    // #1 Variant
-    void Setup() => DI.Setup(nameof(ClocksComposition), kind: CompositionKind.Internal)
-        .SpecialType<ScriptableObject>()
+    void Setup() => DI.Setup(kind: CompositionKind.Internal)
         .Transient(() => clockConfig)
         .Singleton<ClockService>();
-
-    // #2 Variant
-    // void Setup() => DI.Setup()
-    //     .SpecialType<ScriptableObject>()
-    //     .Transient(() => clockConfig)
-    //     .Singleton<ClockService>()
-    //     .Root<IClockService>(nameof(IClockService), kind: RootKinds.Exposed);
 }
 
 public partial class Scope : MonoBehaviour
 {
-    [SerializeField] public ClocksComposition clocksComposition;
-
     void Setup() => DI.Setup()
-        .DependsOn(nameof(ClocksComposition)) // #1 Variant
-        // .Bind().To(_ => clocksComposition) // #2 Variant
-        .SpecialType<MonoBehaviour>()
-        .Root<ClockManager>(nameof(ClockManager))
-        .Builders<MonoBehaviour>();
+        .DependsOn(nameof(ClocksComposition), SetupContextKind.Members)
+        .Root<ClockManager>(nameof(ClockManager));
 
     void Start()
     {
---
+++ samples/UnityApp/Assets/Scripts/UnityGlobal.cs
@@ -0,0 +1,10 @@
+﻿using Pure.DI;
+using UnityEngine;
+
+class UnityGlobal
+{
+    void Setup() => DI.Setup(kind: CompositionKind.Global)
+        .SpecialType<ScriptableObject>()
+        .SpecialType<MonoBehaviour>()
+        .Builders<MonoBehaviour>();
+}
